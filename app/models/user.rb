dc81f03c065d70c2209e8643f1bfacb7cf6fac75
DeLaina <toymom@toymom.com>
2012-05-14 19:12:19 -0500

    worked on master branch this commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,11 +37,17 @@ class User < ActiveRecord::Base
   
   # ---------------------------------------------------------
     
-   def User.authenticate(email, submitted_password)
+   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
+    if user.nil?
+      nil
+    elsif user.has_password?(submitted_password)
+      user
+    else
+      nil
+    end
   end
+
   
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
